67529ef51e15a554e3f9309ef7fc6617cf6bc8bf
sachin-kiwi <sachin.bisht@kiwitech.com>
2023-04-28 12:15:30 +0530

    WIP Alertv2

--- src/helper/screens/alertScreen.js
+++ src/helper/screens/alertScreen.js
@@ -31,3 +31,38 @@ export default class Alert {
     }, 5000);
   }
 }
+
+// New Implementation for alers (WIP)
+export  class Alertv2 {
+  static showAlert(appId,message,type='error') {
+    const element  = createAlertComponent(appId)
+    element.textContent = message;
+    element.color = alertColorSchema.hasOwnProperty(type) 
+      ? alertColorSchema[type] : 'black'
+    element.style.display = 'block';
+    setTimeout(() => {
+        element.textContent = '';
+        element.style.display = 'none';
+    }, 5000);
+  }
+
+}
+
+
+ const createAlertComponent = (appId)=>{
+  const element = document.getElementById(`alert-popup-${appId}`)
+  const mainScreen = document.getElementById(`appScreen-${appId}`) || document.body
+  if (!element){
+      element = document.createElement('div');
+      element.id=`alert-popup-${appId}`;
+      element.style.display = 'none';
+      element.style.position = 'fixed';
+      element.style.top = '10px';
+      element.style.right = '10px';
+      element.style.backgroundColor = '#fff';
+      element.style.border = '1px solid #ccc';
+      element.style.padding = '10px';
+      mainScreen.appendChild(element)
+  }
+  return element
+ }
